ddc24759731c3e2d573b4c430a374d38bff51198
Ahmed Mohamed <ahmedmohamed@Ahmeds-MacBook-Pro.local>
2022-10-10 02:47:38 -0400

    Converted DHL API key into a local environment variable

--- utils/dhl.js
+++ utils/dhl.js
@@ -7,7 +7,7 @@ module.exports = async function dhlData(trackingNumber) {
     Accept: "application/json",
     "User-Agent":
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-    "DHL-API-Key": "2P5rR6AfvkVeqYwjkHwZcWSQGruCO7w0",
+    "DHL-API-Key": process.env.DHL_KEY,
   };
 
   const response = await axios.get(API_URL, {
@@ -25,9 +25,9 @@ module.exports = async function dhlData(trackingNumber) {
     delivered: status.status === "delivered",
     status: status,
     shippedTimestamp: events[0],
-    expectedDeliveryTimestamp: events[-1],
-    attemptedDeliveryDate: events[-1],
-    deliveryDate: events[-1],
+    expectedDeliveryTimestamp: events[events.length -1],
+    attemptedDeliveryDate: events[events.length -1],
+    deliveryDate: events[events.length -1],
     events: events,
     origin: origin,
     destination: destination,
